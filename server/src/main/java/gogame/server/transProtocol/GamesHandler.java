b6eaa2b68b87eac26b083fcdcfb697d23de28a78
Marcin <marcinsz9045@gmail.com>
2019-12-03 00:09:08 +0100

    Initial commit.

---
+++ server/src/main/java/gogame/server/transProtocol/GamesHandler.java
@@ -0,0 +1,9 @@
+package gogame.server.transProtocol;
+/**
+ * Abstract handler
+ * @author marcin
+ * Operates on threads
+ */
+public abstract class GamesHandler implements Runnable{
+	
+}
---
+++ server/src/main/java/gogame/server/transProtocol/TransferProtocol.java
@@ -0,0 +1,21 @@
+package gogame.server.transProtocol;
+/**
+ * Public interface for different transmission protocols
+ * @author marcin
+ *
+ */
+public interface TransferProtocol {
+	/*
+	 * Initialize connection 
+	 */
+	public void initialize();
+	/*
+	 * Sends message to the client
+	 */
+	public void sendMessage();
+	/*
+	 * Receives message from client
+	 */
+	public void recvMessage();
+	
+}
